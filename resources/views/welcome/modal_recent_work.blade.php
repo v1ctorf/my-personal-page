30a7eaee781cb7a3fdfb6dd23324b4e3c5e38ae7
Victor F <mail@victorf.ch>
2019-03-16 03:20:28 +0000

    images ok and new modals

--- resources/views/welcome/modal_recent_work.blade.php
+++ resources/views/welcome/modal_recent_work.blade.php
@@ -3,19 +3,19 @@
 $recent_works = [
     [
         'title' => 'Zygä',
-        'body' => 'Zygä is a bike registry and sharing platform, aimed to increase the use of the already existing resources, such as bicycles, encouraging their repair. In this project, I acted as Technical Lead, System Design and Full-Stack Developer.',
+        'body' => 'Zygä is a bike registry and sharing platform, aimed to increase the use of the already existing resources, such as bicycles, encouraging their repair. In this temporary project, I acted remotely as Technical Lead, System Design and Full-Stack Developer.',
         'url' => 'https://zyga.zone',
         'tech' => ['PHP 7.1', 'Laravel 5.6', 'Eloquent', 'JavaScript', 'jQuery', 'CSS3', 'HTML5', 'Bootstrap', 'Git', 'MySQL']
     ],
     [
         'title' => 'Korjuu',
-        'body' => 'Based in Helsinki, Korjuu is a P2P market platform, aiming to minimize overproduction and wastage of agricultural products and services. In this project, I acted as Technical Lead, System Design and Full-Stack Developer.',
+        'body' => 'Based in Helsinki, Korjuu is a P2P market platform, aiming to minimize overproduction and wastage of agricultural products and services. In this temporary project, I acted remotely as Technical Lead, System Design and Full-Stack Developer.',
         'url' => 'https://korjuu.com',
         'tech' => ['PHP 7.1', 'Laravel 5.7', 'Eloquent', 'JavaScript', 'jQuery', 'CSS3', 'HTML5', 'Bootstrap', 'Git', 'MySQL']
     ],
     [
         'title' => 'DuoSystem',
-        'body' => 'DuoSystem is a company focused for public health management systems in some brazilian states such as São Paulo, Goiás and Mato Grosso do Sul. I was part of their team as Full-Stack Web Developer, using Symfony for new projects, but also maintaining a legacy system.',
+        'body' => 'DuoSystem is a company focused on public health management systems in some brazilian states such as São Paulo, Goiás and Mato Grosso do Sul. I was part of their team as Full-Stack Web Developer, using Symfony for new projects, but also maintaining legacy systems, solving issues and implementing enhancements.',
         'url' => 'https://duosystem.com.br',
         'tech' => ['PHP', 'Symfony', 'JavaScript', 'jQuery', 'CSS3', 'HTML5', 'Bootstrap', 'Git', 'MS SQL Server']
     ],
@@ -36,14 +36,13 @@ $recent_works = [
         aria-labelledby="Modal_{{ ($key+1) }}_Label" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="Modal_{{ ($key+1) }}_Label">
-                        {{ $work['title'] }}
-                    </h5>
-                </div>
                 <div class="modal-body">
                     <img class="img-fluid mb-2" src="img/portfolio/thumbnails/{{ ($key+1) }}.jpg" alt="">
-                    {{ $work['body'] }}
+                    <h5 class="modal-title mt-2 mb-1" id="Modal_{{ ($key+1) }}_Label">
+                        {{ $work['title'] }}
+                    </h5>
+                    
+                    <p class="text-secondary">{{ $work['body'] }}</p>
                     <div class="row mt-2">
                         <div class="col-md-12 text-center">
                             @foreach ($work['tech'] as $tech)
--- resources/views/welcome/recent_work.blade.php
+++ resources/views/welcome/recent_work.blade.php
@@ -8,11 +8,11 @@
                 <a class="portfolio-box" href="javascript:;" data-toggle="modal" data-target="#Modal_1">
                     <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
                     <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">
-                            Laravel (PHP)
-                        </div>
                         <div class="project-name">
                             Zygä
+                        </div>
+                        <div class="project-category text-white-50">
+                            Laravel (PHP)
                         </div>
                     </div>
                 </a>
@@ -21,11 +21,11 @@
                 <a class="portfolio-box" href="javascript:;" data-toggle="modal" data-target="#Modal_2">
                     <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
                     <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">
-                            Laravel (PHP)
-                        </div>
                         <div class="project-name">
                             Korjuu
+                        </div>
+                        <div class="project-category text-white-50">
+                            Laravel (PHP)
                         </div>
                     </div>
                 </a>
@@ -34,11 +34,11 @@
                 <a class="portfolio-box" href="javascript:;" data-toggle="modal" data-target="#Modal_3">
                     <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
                     <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">
-                            Symfony (PHP)
-                        </div>
                         <div class="project-name">
                             DuoSystem
+                        </div>
+                        <div class="project-category text-white-50">
+                            Symfony (PHP)
                         </div>
                     </div>
                 </a>
@@ -47,11 +47,11 @@
                 <a class="portfolio-box" href="javascript:;" data-toggle="modal" data-target="#Modal_4">
                     <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
                     <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">
-                            Grails (Java & Groovy)
-                        </div>
                         <div class="project-name">
                             Victory Consulting
+                        </div>
+                        <div class="project-category text-white-50">
+                            Grails (Java & Groovy)
                         </div>
                     </div>
                 </a>
