83922c9dbdced4faeef0164dc04adeb2dc2e0198
Victor F <mail@victorf.ch>
2019-03-16 22:02:03 +0000

    adjustment at contacts

--- resources/views/welcome/contact.blade.php
+++ resources/views/welcome/contact.blade.php
@@ -5,15 +5,20 @@
             <div class="col-md-10 offset-md-1 text-center">
                 <h2 class="mt-0">{{ __('Contact') }}</h2>
                 <hr class="divider my-4">
-                <p class="text-secondary text-left">If you have a question or are interested in developing an application with web technology, I'd be happy to help you. As a full-stack developer I can bring a project from concept to completion. </p>
-                <p class="text-secondary text-left">I work primarily with <a 
+                <p class="text-secondary text-left">
+                    If you have a question or are interested in developing an application with web technology, I'd be happy to help you. As a full-stack developer I can bring a project from concept to completion. 
+                    I work primarily with <a 
                     href="https://laravel.com" target="_blank" class="text-dark">
                     <u>Laravel</u></a> and <a href="https://grails.org" target="_blank" 
                     class="text-dark"><u>Grails</u></a> on the back-end and <a 
                     href="https://getbootstrap.com" target="_blank" class="text-dark"><u>Bootstrap</u></a> and <a 
-                    href="https://reactjs.org" target="_blank" class="text-dark"><u>React</u></a> on the front-end but picking up new languages or frameworks isn’t a problem. I also appreciate <a 
+                    href="https://reactjs.org" target="_blank" class="text-dark"><u>React</u></a> on the front-end but picking up new languages or frameworks isn’t a problem.
+                </p>
+                <p class="text-secondary text-left"> 
+                    I appreciate <a 
                     href="https://en.wikipedia.org/wiki/Lean_software_development" target="_blank" class="text-dark">
-                    <u>lean software development</u></a>, so if you think the best way to prove an idea is turning it into a reality in the simplest way possible and learn more about your users from the results, let's get in touch.</p>
+                    <u>lean software development</u></a>, so if you also think the best way to prove an idea is turning it into a reality in the simplest way possible and learn more about your users from the results, let's get in touch.
+                </p>
                 <a class="d-block text-left" href="mailto:mail@victorf.ch?subject=Hi">
                     <h5><u>mail@victorf.ch</u></h5>
                 </a>
