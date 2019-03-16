976fa5914803af57ab7e6330063bc555971d0170
Victor F <mail@victorf.ch>
2019-03-16 01:31:03 +0000

    descripting works

--- resources/views/welcome/modal_recent_work.blade.php
+++ resources/views/welcome/modal_recent_work.blade.php
@@ -3,22 +3,22 @@
 $recent_works = [
     [
         'title' => 'Zygä',
-        'body' => 'Tommi, bla bla bla bikes',
+        'body' => 'Zygä is a bike registry and sharing platform, aimed to increase the use of the already existing resources, such as bicycles, encouraging their repair. In this project, I acted as Technical Lead, System Design and Full-Stack Developer.',
         'url' => 'https://zyga.zone'
     ],
     [
         'title' => 'Korjuu',
-        'body' => 'Tommi, bla bla bla my groups',
+        'body' => 'Based in Helsinki, Korjuu is a P2P market platform, aiming to minimize overproduction and wastage of agricultural products and services. In this project, I acted as Technical Lead, System Design and Full-Stack Developer.',
         'url' => 'https://korjuu.com'
     ],
     [
         'title' => 'DuoSystem',
-        'body' => 'Cross, bla bla bla sms sergio',
+        'body' => 'DuoSystem is a company focused for public health management systems in some brazilian states such as São Paulo, Goiás and Mato Grosso do Sul. I was part of their team as Full-Stack Web Developer, using Symfony for new projects, but also maintaining a legacy system.',
         'url' => 'https://duosystem.com.br'
     ],
     [
         'title' => 'Victory Consulting',
-        'body' => 'Dona Vera, bla bla bla VS',
+        'body' => 'Victory is an employee benefits brokerage firm with hundreds of customers in Brazil, including Bridgestone, DHL, Toyota, PepsiCo, etc. I was acting as Full-Stack Developer of a benefits management application built in Grails (Java and Groovy), specially designed for HR users.',
         'url' => 'https://www.victorysolutions.com.br/principal/login/'
     ],
 ];
