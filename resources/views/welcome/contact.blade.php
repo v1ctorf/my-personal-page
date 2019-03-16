5a9bb8145e0bec87dde37278baa81ad722061ca7
Victor F <mail@victorf.ch>
2019-03-16 21:29:33 +0000

    more responsive footer

--- resources/views/welcome/contact.blade.php
+++ resources/views/welcome/contact.blade.php
@@ -1,11 +1,12 @@
- <!-- The best way to prove an idea is turning it into a reality in the simplest way possible. -->
 <!-- Contact Section -->
 <section class="page-section" id="contact">
     <div class="container">
         <div class="row">
-            <div class="col-md-12 text-center">
+            <div class="col-md-8 offset-md-2 text-center">
                 <h2 class="mt-0">{{ __('Contact') }}</h2>
                 <hr class="divider my-4">
+                <p>Have a question or want to work together? Well, If you have an application you are interested in developing with web technology, I’d love to work with you on it. I’m a full-stack developer which means I can bring your project from concept to completion. I work primarily with Django and Node.js on the back-end and Ember.js on the front-end but picking up new languages or frameworks isn’t a problem. </p>
+                <p>The best way to prove an idea is turning it into a reality in the simplest way possible. Let's get in touch!</p>
                 <a class="d-block" href="mailto:mail@victorf.ch?subject=Hi">mail@victorf.ch</a>
             </div>
         </div>
