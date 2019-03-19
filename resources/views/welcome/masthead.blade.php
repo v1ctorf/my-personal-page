0ecad8bb835fec600162e97451f4be0505ed772b
Victor F <mail@victorf.ch>
2019-03-19 22:31:15 +0000

    masthead more responsive for xs screens

--- resources/views/welcome/masthead.blade.php
+++ resources/views/welcome/masthead.blade.php
@@ -3,11 +3,18 @@
     <div class="container h-100">
         <div class="row h-100 align-items-center justify-content-center text-center">
             <div class="col-lg-10 align-self-end">
-                <h1 class="text-white font-weight-bold">
+                <h1 class="text-white font-weight-bold d-none d-sm-block">
                     Olá, {{ __('I\'m Victor Ferreira Silva,')}}
                     <br>{{__('a web developer from Brazil')}}
                     <br>{{__('currently based in Dublin.')}}
-                </h1>
+                </h1>                
+                <h3 class="text-white font-weight-bold d-block d-sm-none">
+                    Olá, {{ __('I\'m')}}
+                    <br>{{ __('Victor Ferreira Silva,')}}
+                    <br>{{__('a web developer')}}
+                    <br>{{__('from Brazil')}}
+                    <br>{{__('currently based in Dublin.')}}
+                </h3>
                 <hr class="divider my-4">
             </div>
             <div class="col-lg-8 align-self-baseline">
