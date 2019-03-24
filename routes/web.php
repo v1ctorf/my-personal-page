65ede36b29d820f9bd010068d8ed0fc344ea96f2
Victor F <mail@victorf.ch>
2019-03-24 17:36:51 +0000

    post model, controller, routes, migration

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/logout', function(){
+    Auth::logout();
+    return redirect('/');
+});
+
 Auth::routes();
 
 Route::redirect('/about', '/#about');
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('post', 'PostController');
+Route::get('/blog', 'PostController@index')->name('blog');
+
 
