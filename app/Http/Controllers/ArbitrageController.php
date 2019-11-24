3485566a955ffb2615426f9cd54542dfe8b6a812
victorf <mail@victorf.ch>
2019-11-24 12:32:55 +0000

    installed guzzle

--- app/Http/Controllers/ArbitrageController.php
+++ app/Http/Controllers/ArbitrageController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client as GuzzleClient;
 
 class ArbitrageController extends Controller
 {
+    private $client;
+
+    public function __construct()
+    {
+        $options = [				
+            'base_uri' => config('app.api_url'),
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ],
+            'timeout'  => 60.0
+        ];
+
+        $this->client = new GuzzleClient($options);
+
+    }
+
     public function index()
     {
         return 'tada';
