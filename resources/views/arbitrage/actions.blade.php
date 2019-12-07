459243ae0147a7c5a17e1325ee1de71f15a0631f
victorf <mail@victorf.ch>
2019-12-07 13:27:56 +0000

    adjusted scenario page

--- resources/views/arbitrage/actions.blade.php
+++ resources/views/arbitrage/actions.blade.php
@@ -1,8 +1,8 @@
 <div class="col-md-6">
     <dl class="row text-secondary">
         @foreach ($scenario->actions as $step => $a)
-            <dt class="col-md-1 text-capitalize">{{ $step + 1 }}</dt>
-            <dd class="col-md-11 text-white">
+            <dt class="col-md-2 text-capitalize">Step {{ $step + 1 }}</dt>
+            <dd class="col-md-10 text-white">
                 {{ $a->type == 'order' ? $a->orderType : $a->type }}
                 @if ($a->type == 'order')
                     {{ strtoupper($a->pair) }} @ {{ 
