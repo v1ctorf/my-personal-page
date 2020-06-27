f36c63af5ac62ac237b37e84f3159a3c39082939
victorf <mail@victorf.ch>
2020-06-27 19:50:33 +0000

    vue installed

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,21 +1,12 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div>
+        <h1>EXAMPLE COMPONENT</h1>
     </div>
 </template>
 
 <script>
     export default {
+        name: "ExampleComponent",
         mounted() {
             console.log('Component mounted.')
         }
